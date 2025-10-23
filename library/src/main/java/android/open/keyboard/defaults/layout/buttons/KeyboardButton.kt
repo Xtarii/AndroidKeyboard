8e882e376f7423763041886046454b053f69919d
Xtarii <carlalvinelias@gmail.com>
2025-10-23 16:28:38 +0200

    better hold button ???
    This is a test in hurry

--- library/src/main/java/android/open/keyboard/defaults/layout/buttons/KeyboardButton.kt
+++ library/src/main/java/android/open/keyboard/defaults/layout/buttons/KeyboardButton.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -43,13 +46,12 @@ fun KeyboardButton(key: String, onHold: () -> Unit = {}, onClick: () -> Unit = {
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
-fun KeyboardButton(onHold: () -> Unit = {}, onClick: () -> Unit = {}, width: Dp = 25.dp, color: Color = Color(0.3f, 0.3f, 0.3f, 0.9f), onHoldInterval: Long = 100, content: @Composable () -> Unit = {}) {
+fun KeyboardButton(onHold: () -> Unit = {}, onClick: () -> Unit = {}, width: Dp = 25.dp, color: Color = Color(0.3f, 0.3f, 0.3f, 0.9f), onHoldInterval: Long = 50, holdDelay: Long  = onHoldInterval, content: @Composable () -> Unit = {}) {
     val scope = rememberCoroutineScope()
     var isHold by remember { mutableStateOf(false) }
 
-    Box(
+    Button(
         modifier = Modifier.height(35.dp).width(width)
-            .background(color = color, shape = RoundedCornerShape(7.dp))
             .pointerInput(Unit) {
                 awaitEachGesture {
                     val down = awaitFirstDown()
@@ -57,6 +59,7 @@ fun KeyboardButton(onHold: () -> Unit = {}, onClick: () -> Unit = {}, width: Dp 
                     onClick() // One Click
 
                     val job = scope.launch {
+                        delay(holdDelay) // Delayed "hold event" start
                         while(isHold) {
                             onHold()
                             delay(onHoldInterval)
@@ -68,7 +71,14 @@ fun KeyboardButton(onHold: () -> Unit = {}, onClick: () -> Unit = {}, width: Dp 
                     job.cancel()
                 }
             },
-        contentAlignment = Alignment.Center,
+
+        shape = RoundedCornerShape(7.dp),
+        colors = ButtonDefaults.buttonColors().copy(
+            containerColor = color
+        ),
+
+        contentPadding = PaddingValues(0.dp),
+        onClick = {}
     ) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             content()
